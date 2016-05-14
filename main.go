2a4b0078ccd10695c21ca49f3da2a3adcaabcb47
Yun Zhi Lin <yun@yunspace.com>
2016-05-14 23:54:27 +1000

    point to deployed API instead of localhost

--- main.go
+++ main.go
@@ -16,15 +16,15 @@ func main() {
                 cmd := os.Args[1]
                 switch cmd {
                 case "all":
-                        request.Get("http://localhost:8080/products").EndBytes(printResponse)
+                        request.Get("https://www.yunspace.com/products").EndBytes(printResponse)
                         return
                 }
-                request.Get("http://localhost:8080/products/" + cmd).EndBytes(printResponse)
+                request.Get("https://www.yunspace.com/products/" + cmd).EndBytes(printResponse)
         }
 
 }
 
-func printResponse(resp gorequest.Response, body string, errs []error){
+func printResponse(resp gorequest.Response, body []byte, errs []error){
         fmt.Printf("response: %d, error: %s\n", resp.StatusCode, errs)
-        fmt.Println(body)
+        fmt.Print(string(body))
 }
